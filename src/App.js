d7ca59aec52c6407498a8d230f4c9172cfabaaee
Akbar Ahmadi <Ahmadi.akbar.1996@gmail.com>
2021-06-19 13:51:03 +0430

    add 404 page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route,BrowserRouter,Switch } from 'react-router-dom';
-// import {  browserHistory } from 'react-router';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { withTranslation } from 'react-i18next';
 
 import routes from './routes';
@@ -15,32 +14,32 @@ import 'swiper/css/swiper.min.css';
 
 import { CheckIos } from './IosStyles';
 CheckIos();
-console.log('window.location.origin',window.location.origin);
-var u='/';
-if(window.globalTS.home_url){
-  u=window.globalTS.home_url.replace(window.location.origin,'');
+console.log('window.location.origin', window.location.origin);
+var u = '/';
+if (window.globalTS.home_url) {
+  u = window.globalTS.home_url.replace(window.location.origin, '');
 }
+
+const page404 = () => <h1>404</h1>;
 
 export default withTranslation()(({ t }) => (
   <div className={t('languageDir')}>
-    <BrowserRouter basename={u}>
-    {/*<Router>*/}
+    <Router basename={u}>
       <Switch>
-      {routes.map((route, index) => (
-        <Route
-          key={index}
-          path={route.path}
-          exact={route.exact}
-          component={(props) => (
-            <route.layout>
-              <route.component {...props} />
-            </route.layout>
-          )}
-        />
-      ))}
+        {routes.map((route, idx) => (
+          <Route
+            key={idx}
+            path={route.path}
+            exact={route.exact}
+            component={(props) => (
+              <route.layout>
+                <route.component {...props} />
+              </route.layout>
+            )}
+          />
+        ))}
+        <Route component={page404} />
       </Switch>
-    </BrowserRouter>
-    {/*</Router>*/}
-
+    </Router>
   </div>
 ));
